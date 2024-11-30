80adbedd3b6d48e53636aeb1babfeb1b00d8b546
swanith1234 <swanithpidugu@gmail.com>
2024-11-30 19:03:02 +0530

    Add folder: src

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App userData={{"name":"musharaf","emailId":"pidugubunny534@gmail.cpm","phoneNo":"34234324324","projects":[{"title":"musharaf","description":"musharaf","technologies":["React"],"repoLink":"musharaf","_id":"674b13446f3ec93e11d3ed9f"}],"resume":"","experiences":[{"companyName":"musharaf","role":"musharaf","description":"musharaf","technologiesUsed":[],"duration":"musharaf","_id":"674b13446f3ec93e11d3eda0"}],"techStacks":[],"contactDetailsUrls":[],"codingProfilesUrls":[],"certificationsUrls":[],"achievements":[{"title":"musharaf","description":"musharaf","_id":"674b13446f3ec93e11d3eda1"}],"about":"musharaf","profilePhoto":"","_id":"674b13446f3ec93e11d3ed9e","createdAt":"2024-11-30T13:29:40.830Z","updatedAt":"2024-11-30T13:29:40.830Z","__v":0}} />
+  </StrictMode>,
+)
